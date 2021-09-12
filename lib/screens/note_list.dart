98a19fa4c1f17577691300f5b7f5851e98fe6a49
xarantolus <xarantolus@protonmail.com>
2021-09-12 17:34:45 +0200

    Refactor Note list

--- lib/screens/note_list.dart
+++ lib/screens/note_list.dart
@@ -20,6 +20,7 @@ class _NoteListState extends State<NoteList> {
   void initState() {
     super.initState();
 
+    // We want to notify the AnimatedList when an item was added or removed
     widget.service.addInsertCallback(
         (index) => widget._listKey.currentState!.insertItem(index));
 
@@ -28,6 +29,103 @@ class _NoteListState extends State<NoteList> {
           (BuildContext context, Animation<double> animation) {
         return Container();
       }),
+    );
+  }
+
+  // _editNote pops up the node editing screen and saves/replaces the note
+  void _editNote(int index, Note item) async {
+    var editedNote = await Navigator.of(context).push(MaterialPageRoute<Note>(
+        builder: (context) => NoteEditPage(item, false)));
+    if (editedNote == null) {
+      return;
+    }
+
+    await widget.service.replaceNote(editedNote, index);
+  }
+
+  void _deleteNote(int index, BuildContext context, Note item) {
+    widget.service.deleteNote(index);
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text('Deleted note'),
+        action: SnackBarAction(
+          label: "Restore",
+          onPressed: () {
+            widget.service.addNote(item, index);
+          },
+        ),
+      ),
+    );
+  }
+
+  // _animatedList creates the animated list with its transitions
+  Widget _animatedList(List<Note> items) {
+    return AnimatedList(
+      key: widget._listKey,
+      initialItemCount: items.length,
+      itemBuilder: (context, index, anim) {
+        final item = items[index];
+
+        return SlideTransition(
+          position: Tween<Offset>(
+            begin: const Offset(-1, 0),
+            end: const Offset(0, 0),
+          ).animate(anim),
+          child: _listCard(index, item, context),
+        );
+      },
+    );
+  }
+
+  // _listCard returns the list card with editing and swiping capability
+  GestureDetector _listCard(int index, Note item, BuildContext context) {
+    return GestureDetector(
+      onTap: () => _editNote(index, item),
+      child: Container(
+        padding: const EdgeInsets.only(left: 8, right: 8, top: 6),
+        child: _dismissibleListCard(item, context, index),
+      ),
+    );
+  }
+
+  // _dismissibleListCard returns a list card that can be swiped away
+  Dismissible _dismissibleListCard(Note item, BuildContext context, int index) {
+    return Dismissible(
+      key: UniqueKey(),
+
+      child: Container(
+        child: NoteCard(note: item),
+        decoration: ShapeDecoration(
+          color: Theme.of(context).cardColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+        ),
+      ),
+
+      // Allow swiping left or right
+      direction: DismissDirection.endToStart,
+
+      // The background behind the list item is a trash can
+      background: Container(
+        child: const Icon(Icons.delete_forever),
+        padding: const EdgeInsets.only(
+          left: 8,
+          right: 8,
+          top: 6,
+        ),
+        decoration: ShapeDecoration(
+          color: Colors.red,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+        ),
+      ),
+
+      onDismissed: (direction) async {
+        _deleteNote(index, context, item);
+      },
     );
   }
 
@@ -41,83 +139,7 @@ class _NoteListState extends State<NoteList> {
             if (snapshot.data!.isEmpty) {
               return const Center(child: Text("Nothing to see here..."));
             } else {
-              return AnimatedList(
-                key: widget._listKey,
-                initialItemCount: snapshot.data!.length,
-                itemBuilder: (context, index, anim) {
-                  final item = snapshot.data![index];
-
-                  return SlideTransition(
-                    position: Tween<Offset>(
-                      begin: const Offset(-1, 0),
-                      end: Offset(0, 0),
-                    ).animate(anim),
-                    child: GestureDetector(
-                      onTap: () async {
-                        var editedNote = await Navigator.of(context).push(
-                            MaterialPageRoute<Note>(
-                                builder: (context) =>
-                                    NoteEditPage(item, false)));
-                        if (editedNote == null) {
-                          return;
-                        }
-
-                        await widget.service.replaceNote(editedNote, index);
-                      },
-                      child: Container(
-                        padding:
-                            const EdgeInsets.only(left: 8, right: 8, top: 6),
-                        child: Dismissible(
-                          key: UniqueKey(),
-
-                          child: Container(
-                            child: NoteCard(note: item),
-                            decoration: ShapeDecoration(
-                              color: Theme.of(context).cardColor,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12.0),
-                              ),
-                            ),
-                          ),
-                          // Allow swiping left or right
-                          direction: DismissDirection.horizontal,
-
-                          background: Container(
-                            child: const Icon(Icons.delete_forever),
-                            padding: const EdgeInsets.only(
-                              left: 8,
-                              right: 8,
-                              top: 6,
-                            ),
-                            decoration: ShapeDecoration(
-                              color: Colors.red,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12.0),
-                              ),
-                            ),
-                          ),
-
-                          onDismissed: (direction) async {
-                            widget.service.deleteNote(index);
-
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: const Text('Deleted note'),
-                                action: SnackBarAction(
-                                  label: "Restore",
-                                  onPressed: () {
-                                    widget.service.addNote(item, index);
-                                  },
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              );
+              return _animatedList(snapshot.data!);
             }
           } else if (snapshot.hasError) {
             return ErrorWidget(snapshot.error!);

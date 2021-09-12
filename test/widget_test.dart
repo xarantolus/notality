ad39ce0117363b25aa20ac17232e43729c07aa5b
xarantolus <xarantolus@protonmail.com>
2021-09-12 22:13:35 +0200

    Add a basic test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,22 +9,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:notality/main.dart';
+import 'package:notality/screens/note_edit.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Floating action button brings up the edit screen',
+      (WidgetTester tester) async {
     await tester.pumpWidget(const NotesApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    await tester.tap(find.byType(FloatingActionButton));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.pumpAndSettle();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // The note edit page should be brought up
+    expect(find.byType(NoteEditPage), findsOneWidget);
   });
 }
